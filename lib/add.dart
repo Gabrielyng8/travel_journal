7d9c7faaf64b813a9c274c68df418b2471b5d8cf
Gabriel Young <gabriel.young.g52202@mcast.edu.mt>
2025-03-10 19:13:15 +0100

    Added basic AddLogScreen widget for log entry functionality

---
+++ lib/add.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AddLogScreen extends StatefulWidget {
+  const AddLogScreen({super.key});
+
+  @override
+  State<AddLogScreen> createState() {
+    return _AddLogScreenState();
+  }
+}
+
+class _AddLogScreenState extends State<AddLogScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Add log screen');
+  }
+}
