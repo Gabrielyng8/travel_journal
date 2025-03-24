faf22a116ce5f1b947185f1b6518df8420635c82
Gabriel Young <gabriel.young.g52202@mcast.edu.mt>
2025-03-24 18:48:55 +0100

    Add constructor to ResponsiveScreen for improved widget initialization

--- lib/responsive_screen.dart
+++ lib/responsive_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class ResponsiveScreen extends StatelessWidget {
+  const ResponsiveScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
